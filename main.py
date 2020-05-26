a4ee39eea4d6f702e74fdffed64f85a9e7de86d8
Keith Downer <keith.downer897@myci.csuci.edu>
2020-05-26 08:24:06 -0700

    Reset the main logic to endlessly spawn particles and added some comments to the main.py file.

--- main.py
+++ main.py
@@ -8,14 +8,16 @@ system_create_times = []
 def mc(timestamp, delta):
 	global system_create_times
 
-	#print(len(particles.systems), len(particles.free_systems))
-	#print(timestamp)
+	# start after 3 seconds
+	if timestamp < 3000:
+		return
 
-	if random.random() < 0.05 and len(particles.systems) < 6 and len(particles.free_systems) == 0 and timestamp > 3000:
+	# create up to 6 particle systems at a time with randomized properties
+	if random.random() < 0.05 and len(particles.systems) - len(particles.free_systems) < 6:
 		system_index = particles.create_particle_system(particles.square_vao, particles.test_program, {
 			"active": True,
 			"particle_count": 100,
-			"spawn_type": "instant",  # "instant" "continuous"  ?
+			"spawn_type": "instant",
 			"lifetime_min": 0,
 			"lifetime_max": 4,
 			"start_velocity_min": [-2, -2, -2],
@@ -27,6 +29,7 @@ def mc(timestamp, delta):
 		})
 		system_create_times.append((system_index, timestamp))
 
+	# clean up systems that have gone past their lifetimes
 	for ct in system_create_times:
 		if (ct[1] + particles.systems[ct[0]]["lifetime_max"]*1000) < timestamp:
 			particles.free_particle_system(ct[0])
@@ -34,7 +37,7 @@ def mc(timestamp, delta):
 
 	#print("System Count: ", len(particles.systems), "\t\tParticle Count: ", len(particles.particles), "\t\t", len(particles.free_systems))
 
-particles.main_callback = mc
 
+particles.main_callback = mc
 
 particles.particle_loop()
